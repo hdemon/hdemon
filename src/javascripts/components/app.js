17a8250b194e340ecbb45d0dcd36d7c20ba29bb3
Masami Yonehara <z.masami.yonehara@gree.net>
2015-03-05 20:30:29 +0900

    show repository list

--- src/javascripts/components/app.js
+++ src/javascripts/components/app.js
@@ -1,14 +1,33 @@
-var React = require('react');
+var React = require('react'),
+    axios = require('axios');
 
 var DisplayItem = React.createClass({
   render: function() {
-    return <div>app</div>;
+    return <a href={this.props.url}>{this.props.name}</a>;
   }
 });
 
 var App = React.createClass({
+  getInitialState: function() {
+    return {repositories: []};
+  },
+
+  componentDidMount: function() {
+    axios.get('https://hdemon-backend.herokuapp.com/api/repositories').then((response) => {
+      this.setState({ repositories: response.data.data });
+    })
+  },
+
   render: function() {
-    return <DisplayItem />;
+    var repositories = this.state.repositories.map((repository) => {
+      return <DisplayItem url={repository.html_url} name={repository.name} />;
+    });
+
+    return (
+      <div>
+        {repositories}
+      </div>
+    )
   }
 });
 
