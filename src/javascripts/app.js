a626723752ccb7bb8d6428643b78667be2f1b5ef
Masami Yonehara <zeitdiebe@gmail.com>
2015-03-08 01:45:21 +0900

    add router

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,11 +1,15 @@
 window.React = require('react');
 window.Promise = require('ypromise');
-var Arda = require('arda');
+var Arda = require('arda'),
+    page = require('page')
 var WorksContext = require('./contexts/works'),
     ArticlesContext = require('./contexts/articles')
 
 window.addEventListener('DOMContentLoaded', () => {
   var router = new Arda.Router(Arda.DefaultLayout, document.querySelector('#app'))
-  // router.pushContext(WorksContext, {})
-  router.pushContext(ArticlesContext, {})
+
+  page.start({hashbang: true})
+  page('/articles', () => { router.pushContext(ArticlesContext, {}) })
+  page('/works', () => { router.pushContext(WorksContext, {}) })
+  page('/articles')
 });
