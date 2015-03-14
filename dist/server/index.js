15adfe97fd9ef00bfe47e5750f7ba35416c8bee4
Masami Yonehara <zeitdiebe@gmail.com>
2015-03-14 20:25:32 +0900

    remove cors header

--- dist/server/index.js
+++ dist/server/index.js
@@ -2,12 +2,10 @@
 
 var fs = require("fs");
 var express = require("express");
-var corser = require("corser");
 var MyArticle = require("./my_article");
 var app = express();
 
 app.set("port", process.env.PORT || 5000);
-app.use(corser.create());
 app.use("/public", express["static"](__dirname + "/../front"));
 
 var myArticle = new MyArticle();
