15f1825c984c76d7fe1dcc340835db0748b652e5
Masami Yonehara <zeitdiebe@gmail.com>
2015-03-14 18:35:30 +0900

    enable cors

--- app/server/index.js
+++ app/server/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var corser = require("corser");
 var MyArticle = require('./my_article');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+app.use(corser.create());
 app.use('/public', express.static(__dirname + '/../front'));
 
 var myArticle = new MyArticle;
